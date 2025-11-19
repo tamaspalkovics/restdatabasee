283164510a1b80924a79aca5e903ec91d4e6036a
tamaspalkovics <134604398+tamaspalkovics@users.noreply.github.com>
2025-11-19 10:26:38 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,62 +1,61 @@
-const e = require('express');
 const express = require('express');
-const sqlite = require('sqlite3').verbose();//hibakeresés
-const cors = require('cors');
-const app= express();
-const port= 3000;
+const sqlite = require('sqlite3').verbose(); //verbose - hibakeresési támogatás
+const cors = require('cors'); 
+const app = express();
+const port = 3000;
 
 
-//Middleware - köztes alkalmazások
+//Middleware - köztes alkalamzások
 app.use(express.json());
 app.use(cors()); //Cross Origin Resource Sharing
 
-
-//az adatbázis 
-const db = new sqlite.Database('users.db', (err) =>{
-    if(err) {
+//Az adatbázis inicializálása
+const db = new sqlite.Database('users.db', (err) => {
+    if (err) {
         console.log(err.message);
     }
     else {
         console.log('Az adatbázis kapcsolat létrejött.')
     }
 })
-//adatbázis séma létrehozása
-db.run(`CREATE TABLE IF NOT EXISTS users(
+
+//Adatbázis séma létrehozása
+db.run(`CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    firstName TEXT NOT NULL,
+    firstName TEXT NUT NULL,
     lastName TEXT NOT NULL,
     city TEXT NOT NULL,
     address TEXT NOT NULL,
     phone TEXT NOT NULL,
     email TEXT NOT NULL,
-    gender TEXT NOT NULL 
+    gender TEXT NOT NULL
     )`);
-//POST végpont az adatok mentésére a users táblában 
-app.post('/api/users',(req,res) =>{
-    //adatok elérése
-    const {firstName, lastName, city, address, phone, email, gender}= req.body;
 
-    const sql = 'INSERT INTO users (firstName, lastName, city, address, phone, email, gender) VALUES(?,?,?,?,?,?,?)'
+//POST végpont az adatok eltárolására a users táblában
+app.post('/api/users', (req, res) => {
+    const { firstName, lastName, city, address, phone, email, gender } = req.body;
+
+    const sql = `INSERT INTO users (firstName, lastName, city, address, phone, email, gender) VALUES (?, ?, ?, ?, ?, ?, ?)`;
     db.run(sql, [firstName, lastName, city, address, phone, email, gender],
-    function(err){
-        if (err) {
-            console.log(err);
-            return res.status(500).json({error: "Hiba történt"});
+        //Callback függvény az esetleges adatbázis kapcsolódási hiba kezelésére, vagy a sikeres adatbázis művelet eredményének a visszaküldésére a kliensnek.
+        function (err) {
+            if (err) {
+                console.log(err);
+                return res.status(500).json({ message: 'Hiba történt az adatok rögzítése során!' })
+            } else {
+                return res.status(201).send({ message: 'Az adatok rögzítése sikeresvolt.', id: this.lastID, firstName, lastName, city, address, phone, email, gender });
+            }
         }
-        else {
-            res.status(200).send({message: 'Adatok sikeresen rögzítése', id: this.lastID,firstName, lastName, city, address, phone, email, gender});
-        }
-    })
+    )
 })
 
-//Get végpont az adatbázis lekérésére
-app.get('/api/users', (req,res) =>{
-   db.all('SELECT * FROM users', [], (err, records) =>{
+//GET végpont az adatbázis lekérésére
+app.get('/api/users', (req, res) => {
+    db.all(`SELECT * FROM users`, [], (err, records) => {
         if (err) {
             console.error(err);
-            return res.status(500).json({message: 'Hiba történt az adatok kiolvasása során'});
-        }
-        else {
+            return res.status(500).json({ message: 'Hiba történt az adatok kiolvasása során!' });
+        } else {
             return res.status(200).json(records);
         }
     })
@@ -64,45 +63,47 @@ app.get('/api/users', (req,res) =>{
 
 //DELETE végpont az adatbázis egy sorának (id szerinti) törlésére
 app.delete('/api/users/:id', (req, res) => {
-    //id elérése az URL paraméter listából
+    //Az id elérése az URL paraméter listájából
     const { id } = req.params;
 
     //Az adatbázis egy sorának a törlése
-    db.run('DELETE FROM users WHERE id = ?', [id],
-    function(err){
-        if (err) {
-            return res.status(500).send(err.message);
-        }
-        else {
-            return res.status(200).send({message: 'Sikeres törlés'});
-        }
-    })
+    db.run(`DELETE FROM users WHERE id = ?`, [id],
+        //Callback föggvény az esetleges törlési hiba kezelésére
+        function (err) {
+            if (err) {
+                return res.status(500).send(err.message);
+            } else {
+                return res.status(200).json({ message: 'Sikeres adattörlés!' });
+            }
+        })
 })
 
-//PUT a tábla adatainak módosítására
+//PUT végpont a táblázat adataink a módosítására
 app.put('/api/users/:id', (req, res) => {
-    //id elérése az URL paraméter listából
-    const { id } = req.params;
-    const { firstName, lastName, city, address, phone, email, gender } = req.body;
+        //Az id elérése az URL paraméter listájából
+        const { id } = req.params;
+        const { firstName, lastName, city, address, phone, email, gender } = req.body;
 
-    const sql = `UPDATE users SET firstName = ?, lastName = ?, city = ?, address = ?, phone = ?, email = ?, gender = ? WHERE id = ?`;
+        const sql = 'UPDATE users SET firstName = ?, lastName = ?, city = ?, address = ?, phone = ?, email = ?, gender = ? WHERE id = ?';
 
-    db.run(sql, [firstName, lastName, city, address, phone, email,  gender, id],
-        //Callback függvény a frissítés eredményének kezelésére
-    function(err) {
-        if (err) {
-            console.error(err);
-            return res.status(500).json(err.message);
-        } else {
-            return res.status(200).json({ message: 'A felhasználó frissítése megtörtént', id, firstName, lastName, city, address, phone, email, gender});
-        }
-    }
-)
+        db.run(sql, [firstName, lastName, city, address, phone, email, gender, id],
+            //Callback föggvény az esetleges adatbázis elérési hiba kezelésére
+            function (err) {
+                if (err) {
+                    return res.status(500).send(err.message);
+                } else {
+                    return res.status(200).json({ message: 'A felhasználó frissítése megtörtént!', id, firstName, lastName, city, address, phone, email, gender });
+                }
+            }
+        )
 })
 
-//Szerver indítása
+app.listen(port, () => {
+    console.log(`A webszerver fut a http://localhost:${port} webcímen`);
+})
 
 
-    app.listen(port, () => {
-        console.log(`A szerver fut a htttp://localhost: ${port} címen`);
-    })
+
+
+
+
